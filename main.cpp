91201dd7e06720f2293898b33e4fe8285d91819e
Mikey352 <mikeesquivel234@me.com>
2021-04-22 14:50:23 -0400

    added comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "funcs.h"
 #include "iterator"
-
+//necessary librarires & header files
 int main()
 {
   std::string line{std::istreambuf_iterator<char>(std::cin), {}};
+  //reads in from cin
   std::string teststring = removeLeadingSpaces(line);
   std::cout << indent(teststring);
   return 0;
+  //tests with main and prints out answers
 }
